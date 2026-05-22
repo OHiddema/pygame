7a015d5513b5d75c3ec5d6ea4f54881d0d11a098
Oege Hiddema <oegehiddema@proton.me>
2026-05-22 21:03:28 +0200

    get rid of redundant vars: game_over, is_paused

--- src/main.py
+++ src/main.py
@@ -198,10 +198,13 @@ class GameState:
         self.pause_monster = Monster("monster.png")
 
         self.score = 0
-        self.game_over = False
-        self.last_monster_move = time.perf_counter()
+
+        # self.game_over = False
         self.pause_state = self.PauseState.NONE
-        self.is_paused = False
+
+        self.last_monster_move = time.perf_counter()
+        # self.is_paused = False
+        self.pause_state = self.PauseState.NONE
         self.pause_end = 0.0
         self.pause_toggle = False
         self.pause_toggle_next = 0.0
@@ -211,11 +214,15 @@ class GameState:
         # Return the current status message based on game state
 
         # robot is caught by a monster
-        if self.game_over:
+        # if self.game_over:
+        if self.pause_state == self.PauseState.MONSTER:
             return STATUS_GAME_OVER
 
         # robot got the coin
-        if self.is_paused and self.pause_state == self.PauseState.COIN:
+        # if self.is_paused and self.pause_state == self.PauseState.COIN:
+
+        # ditkanbeter
+        if self.pause_state != self.PauseState.NONE and self.pause_state == self.PauseState.COIN:
             return STATUS_GOT_IT
 
         # waiting until robot makes first move
@@ -264,7 +271,8 @@ class GameState:
             self._place_at(mx, my, m)
 
     def update(self):
-        if self.is_paused:
+        # if self.is_paused:
+        if self.pause_state != self.PauseState.NONE:
 
             now = time.perf_counter()
             # Toggle coin/monster on top vs robot on top
@@ -273,20 +281,23 @@ class GameState:
                 self.pause_toggle_next = now + self.pause_toggle_interval
             if self.pause_state == self.PauseState.COIN:
                 if now >= self.pause_end:
-                    self.is_paused = False
+                    # self.is_paused = False
+                    self.pause_state = self.PauseState.NONE
                     if len(self.monsters) < MAX_MONSTERS:
                         self.monsters.append(Monster("monster.png"))
                     self.setup_entities()
             return
 
         # ghosts start moving after first move from robot is made
-        if not self.game_over and self.robot.made_first_move:
+        # if not self.game_over and self.robot.made_first_move:
+        if not self.pause_state == self.PauseState.MONSTER and self.robot.made_first_move:
             self.check_coin_collision()
             self.check_monster_collisions()
             # -------------------------------------------------
             # don't perform this check if there was a monster collection
             # otherwise the ghost that collided could move away from the robot again!
-            if not self.game_over:
+            # if not self.game_over:
+            if not self.pause_state == self.PauseState.MONSTER:
                 self.check_time_to_move_monsters()
             # -------------------------------------------------
 
@@ -318,7 +329,9 @@ class GameState:
 
         # -- Pause: flip robot vs coin/ghost on top --
         self.coin.draw_CC(self.screen)
-        if self.is_paused:
+
+        #ditkanbeter
+        if self.pause_state != self.PauseState.NONE:
             if self.pause_state == self.PauseState.COIN:
                 top, bottom = (
                     (self.coin, self.robot)
@@ -357,16 +370,12 @@ class GameState:
         if (self.robot.x, self.robot.y) == (self.coin.x, self.coin.y):
             self.score += 1
             self.robot.reset_first_move_flag()
-
-            self.is_paused = True
             self.pause_state = self.PauseState.COIN
             self.pause_end = time.perf_counter() + self.pause_time_after_coin_catch
 
     def check_monster_collisions(self):
         for m in self.monsters:
             if self.robot.x == m.x and self.robot.y == m.y:
-                self.game_over = True
-                self.is_paused = True
                 self.pause_state = self.PauseState.MONSTER
                 self.pause_monster = m
                 return
@@ -390,14 +399,14 @@ def main():
                 running = False
 
             # Check for Restart Key (R) ONLY if game is over
-            if state.game_over and event.type == pygame.KEYDOWN:
+            if state.pause_state == state.PauseState.MONSTER and event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     print("Restarting game...")
                     state.reset()  # <-- This is the magic line
                     continue  # Skip the rest of the loop for this frame to avoid double-input issues
 
-            # if not state.game_over:
-            if not state.game_over and not state.is_paused:
+            # ditkanbeter
+            if not state.pause_state == state.PauseState.MONSTER and not state.pause_state != state.PauseState.NONE:
                 if event.type == pygame.KEYDOWN:
                     key = event.key
                     if key == pygame.K_LEFT:
