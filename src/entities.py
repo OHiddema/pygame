322107ce585d80d6b439300d276e474a35b9cf5f
Oege Hiddema <oegehiddema@proton.me>
2026-05-29 10:26:37 +0200

    re-introduce single time warning for missing file

--- src/entities.py
+++ src/entities.py
@@ -5,11 +5,14 @@ from pathlib import Path
 from settings import *
 from models import Position
 
+# track if we've already warned about missing images
+warned_missing_images = set()
+
 
 # loader function that tries to load and scale the image
 def load_and_scale_image(path, field_size) -> pygame.Surface:
     path = Path(__file__).resolve().parent / "assets" / path
-    if not os.path.exists(path):
+    if not path.exists():
         raise FileNotFoundError(f"Image file not found: {path}")
     image = pygame.image.load(path).convert_alpha()
     w, h = image.get_size()
@@ -19,15 +22,15 @@ def load_and_scale_image(path, field_size) -> pygame.Surface:
     return pygame.transform.scale(image, (new_w, new_h))
 
 
-def make_image(
-    filename: str, fallback_color: tuple[int, int, int], field_size: int = CELL_SIZE
-) -> pygame.Surface:
+def make_image(filename: str, fallback_color: tuple[int, int, int], field_size: int = CELL_SIZE) -> pygame.Surface: # fmt: skip
     try:
         return load_and_scale_image(filename, field_size)
     except FileNotFoundError:
         # fallback to colored boxes when image files could not be loaded
         filename = os.path.basename(filename)
-        print(f"⚠️  Missing file: '{filename}'. Using a colored box instead.")
+        if filename not in warned_missing_images:
+            print(f"⚠️  Missing file: '{filename}'. Using a colored box instead.")
+            warned_missing_images.add(filename)
         surface = pygame.Surface((field_size, field_size))
         surface.fill(fallback_color)
         return surface
@@ -69,9 +72,7 @@ class Monster(Entity):
         super().__init__(pos, image)
         self.next_move_time = 0.0
 
-    def move_intelligent(
-        self, legal_deltas: list[tuple[int, int]], robot_pos: Position
-    ):
+    def move_intelligent(self, legal_deltas: list[tuple[int, int]], robot_pos: Position): # fmt: skip
         if not legal_deltas:  # an empty list evaluates to False
             return
 
@@ -79,14 +80,8 @@ class Monster(Entity):
         is_smart = random.random() < MONSTER_IQ / 100
 
         if is_smart:
-            min_distance = min(
-                (self.pos + d).distance_to(robot_pos) for d in legal_deltas
-            )
-            best_moves = [
-                d
-                for d in legal_deltas
-                if (self.pos + d).distance_to(robot_pos) == min_distance
-            ]
+            min_distance = min((self.pos + d).distance_to(robot_pos) for d in legal_deltas) # fmt: skip
+            best_moves = [d for d in legal_deltas if (self.pos + d).distance_to(robot_pos) == min_distance] # fmt: skip
             new_pos = self.pos + random.choice(best_moves)
         else:
             new_pos = self.pos + random.choice(legal_deltas)
